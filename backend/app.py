40ef4ac3360c5bda02289e6880fe053dedacfc6b
rodrigomscmontenegro <rodrigodecastromontenegro@gmail.com>
2026-03-04 11:45:36 -0300

    Adicionei a função criar_prompt

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,21 @@ client = genai.Client(api_key=geminiKey) # Inicialização do Cliente Gemini
 
 #-------
 
+def criar_prompt(texto, max_frases = 5):
+    """
+    Gera o prompt focado em acessibilidade.
+    Linguagem literal, frases curtas e sem distrações visuais(markdowns)
+    """
+    return (
+        f"Você é a OlivIA, uma assistente didática e acolhedora. "
+        f"Seu objetivo é explicar o texto abaixo para uma criança de 10 a 13 anos com TDAH ou TEA. "
+        f"Siga estas regras estritamente:\n"
+        f"1. Use linguagem clara, direta e sem metáforas.\n"
+        f"2. Use no máximo {max_frases} frases curtas.\n"
+        f"3. Não use negrito, itálico, asteriscos ou qualquer formatação markdown.\n"
+        f"4. Foque apenas na ideia principal e use palavras simples.\n\n"
+        f"Texto para simplificar: {texto}")
+
 
 @app.route('/')
 def home():
@@ -29,9 +44,9 @@ def simplify():
     print(f"Recebi {len(texto_recebido)} caracteres para processar!")
 
     response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=f"Você é OlívIA uma assistente de estudos para criaças de 5 a 10 anos com TEA e TDAH, explique de maneira simplificada em ate 5 frases (não responda com markdown): {texto_recebido}"
-        )
+            model = "gemini-2.5-flash", 
+            contents = criar_prompt(texto_recebido, max_frases = 5)
+    )
 
     print(response.text)
 
@@ -49,9 +64,9 @@ def explain():
     print(f"Recebi {len(texto_recebido)} caracteres para processar!")
 
     response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=f"Você é OlívIA uma assistente de estudos para criaças de 5 a 10 anos com TEA e TDAH, explique de maneira simplificada em até 3 frases: {texto_recebido}"
-        )
+            model = "gemini-2.5-flash", 
+            contents = criar_prompt(texto_recebido, max_frases = 3)
+    )
 
     print(response.text)
 
